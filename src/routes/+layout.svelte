3fc73769c280d90cc8d81c424ab90ae45a3d580d
Oscar-4321 <oblewitt@icloud.com>
2023-06-01 14:44:42 +0100

    Add verecel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,12 @@
     import "../app.css";
     import NavBar from "../components/NavBar.svelte";
     import "@fontsource/zen-dots";
-</script>
+    import { inject } from "@vercel/analytics"
+    import { dev } from "$app/environment"
 
+    inject({mode: dev ? 'development' : 'production'})
+
+</script>
 
 <div class="h-screen overflow-y-hidden">
     <NavBar />
