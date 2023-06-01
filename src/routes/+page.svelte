af6540b5ef8d659dc4765b23387ce24ccf431883
Oscar-4321 <oblewitt@icloud.com>
2023-06-01 12:43:52 +0100

    changed something

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1 class="text-4xl">Welcome to Fur Dogs</h1>
+<h1 class="text-4xl">Welcome to Fur Dogs!</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
